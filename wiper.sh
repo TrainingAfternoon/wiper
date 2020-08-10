6de5bd979322a46f6dc99a5364cedbd335eea49d
TrainingAfternoon <samkeyser1@gmail.com>
2020-08-10 11:20:25 -0500

    added setting_menu functionality

--- wiper.sh
+++ wiper.sh
@@ -2,9 +2,31 @@
 VERSION="v1.0"
 LINES=$(tput lines)
 COLUMNS=$(tput cols)
+
+#Global Variables
+PATTERN="sp0"
+
 #need to get output from lsblk -pld -o NAME,SIZE -e7
+#DUMP=$(lsblk -pld -o NAME,SIZE -e7 | grep "sd*") works for variable-ifying
+
 #progress bar for dd can be created with:
 #https://www.cyberciti.biz/faq/linux-unix-dd-command-show-progress-while-coping/
+#pv -n /dev/urandom | dd of=$TARGET_DISK bs=$BS | whiptail \
+#--title "Wiping "$TARGET_DISK --gauge "dd: Dunkin' hDds" 6 50 0
+#display different phase messages
+#https://stackoverflow.com/questions/40989842/how-to-display-different-messages-on-whiptail-progress-bar-along-with-progress-b/40995466#40995466
+
+function setting_menu {
+  echo "Made it here!"
+  PATTERN=$(whiptail --title "Settings" --radiolist "Choose a wipe pattern (press space to select):" \
+  $LINES $((COLUMNS-4)) $(($LINES - 8)) \
+  "sp0" "2-pass 0/verify " OFF \
+  "spff" "2-pass ff/verify " OFF \
+  "dod" "[RECOMMENDED] 4-pass r/0/ff/verify " ON \
+  "nnsa" "4-pass r/r/0/verify " OFF \
+  "bsi" "9-pass ff/fe/fd/fb/f7/ef/df/bf/7f + verify " OFF \
+  3>&1 1>&2 2>&3) && echo $PATTERN && main_menu
+}
 
 function main_menu {
   TITLE="wiper "$VERSION
@@ -12,15 +34,18 @@ function main_menu {
   MENOPT=$(whiptail --title "$TITLE" --menu "Choose an option:" \
   $LINES $COLUMNS $(($LINES - 8)) \
   "Wipe" "Select drive to wipe using specified pattern" \
-  "Settings" "Modify wipe patterns" 3>&1 1>&2 2>&3)
+  "Settings" "Modify wipe patterns" \
+  "Exit" "Exit wiper" 3>&1 1>&2 2>&3)
 
   case $MENOPT in
     "Wipe")
       echo "Wiped"
     ;;
     "Settings")
-      echo "Set"
+      setting_menu
     ;;
+    "Exit")
+      echo "Wiper exited successfully! Thank you for using!"
   esac
 }
 
